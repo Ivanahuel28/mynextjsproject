4d8506ebc200027981c3abafdbb2f6826098c443
Ivan Gomez <ivanahuel.ing@gmail.com>
2024-03-06 13:00:40 -0300

    layouts files

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,14 +1,12 @@
-import Navbar from "../components/Navbar"
+import Navbar from "../components/Navbar.jsx"
 
 export default function RootLayout({ children }) {
  return (
     <html lang="en">
       <body>
-      
         <Navbar />
 
         {children}
-
       </body>
     </html>
   )
---
+++ app/tienda/layout.jsx
@@ -0,0 +1,13 @@
+export default function TiendaLayout({children}) {
+    return <>
+    <nav>
+        <h3>Seccion Tienda</h3>
+        <ul>
+            <li>Categorias</li>
+            <li>Productos</li>
+        </ul>
+    </nav>
+    {children}
+    </>;
+}
+   
